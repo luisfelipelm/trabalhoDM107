addbfdad79d474c0a889106be3435eb94027d966
Crislaine <crislaine@inatel.br>
2015-10-30 10:06:03 -0200

    change report tablem add - where have null value

--- client/js/TableController.js
+++ client/js/TableController.js
@@ -13,19 +13,20 @@ var TableController = {
             $("tr:has(td)").remove();
             
             list.forEach(function(delivery) {
+           
                // Create a new row and append the columns
                 $("#added-articles").append($('<tr/>')
                         .append('<td>' + delivery.id_pedido + '</td>')
                         .append('<td>' + delivery.id_cliente + '</td>')
-                        .append('<td>' + delivery.nome_recebedor + '</td>')
-                        .append('<td>' + delivery.cpf_recebedor + '</td>')
+                        .append('<td>' + (delivery.nome_recebedor == null ? "-" : delivery.nome_recebedor) + '</td>')
+                        .append('<td>' + (delivery.cpf_recebedor == null ? "-" : delivery.cpf_recebedor) + '</td>')
                         .append('<td>' + delivery.endereco + '</td>')
                         .append('<td>' + delivery.bairro + '</td>')
                         .append('<td>' + delivery.cidade + '</td>')
                         .append('<td>' + delivery.estado + '</td>')
                         .append('<td>' + delivery.pais + '</td>')
                         .append('<td>' + Boolean(delivery.recebedor_entregador) + '</td>')
-                        .append('<td>' + delivery.data_entrega + '</td>')
+                        .append('<td>' + (delivery.data_entrega == null ? "-" : delivery.data_entrega) + '</td>')
                         .append('<td>' + delivery.localizacao_geografica + '</td>')
                         .append('<td>' + delivery.status + '</td>'));	
             });     
